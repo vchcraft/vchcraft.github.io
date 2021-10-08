e7d8fd09f4fbff67686567de57ac2fa92f74971b
vchcraft <vcherny2009@gmail.com>
2021-10-08 15:09:30 +0300

    Update load.js

--- load.js
+++ load.js
@@ -9,7 +9,9 @@
 
 sitename = document.getElementsByClassName("sitename")[0];
 menublock = document.getElementsByClassName("menu-block-inner")[0];
-	  
+description = document.getElementsByName("description")[0];
+keywords = document.getElementsByName("keywords")[0];
+
 /*load_info = new XMLHttpRequest();
 	  
 load_info.onreadystatechange = function () {
@@ -48,3 +50,8 @@ load_menublock.send();
 
 sitename.innerHTML = "<a href='index.html'>Социальные и гуманитарные науки. <br> Отечественная и зарубежная литература. <br> Серия 7. Литературоведение </a>"
 
+description.content = "Социальные и гуманитарные науки. Отечественная и зарубежная литература. Серия 7. Литературоведение. Научный журнал, изаваемый ИНИОН РАН"
+
+keywords.content = "наука, журнал, научный журнал, социальные, гуманитарные, отечественная, зарубежная, иностранная, русская, литература, литературоведение, гуманитарная наука, инион"
+
+document.title = description.content
